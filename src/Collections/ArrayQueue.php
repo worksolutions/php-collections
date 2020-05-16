d08dbae6d3a164d4a592ee31040e558ee4f5ea5b
Igor Pomiluyko <pomiluyko@worksolutions.ru>
2020-05-16 16:24:04 +0300

    ArrayQueue implementation

---
+++ src/Collections/ArrayQueue.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @author  Igor Pomiluyko pomiluyko@worksolutions.ru
+ * @license MIT
+ */
+
+namespace WS\Utils\Collections;
+
+
+class ArrayQueue extends AbstractList implements Queue
+{
+
+    public function offer($element)
+    {
+        $this->add($element);
+    }
+
+    public function poll()
+    {
+        if ($this->isEmpty()) {
+            return null;
+        }
+
+        return array_shift($this->elements);
+    }
+
+    public function peek()
+    {
+        if ($this->isEmpty()) {
+            return null;
+        }
+
+        return $this->elements[0];
+    }
+
+
+    public function stream(): Stream
+    {
+        // TODO: Implement stream() method.
+    }
+}
