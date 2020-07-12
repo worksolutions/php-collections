e0978fa21dda25dedeb58402ec40cb6e9b4dc9e0
Максим Соколовский <sokolovsky@worksolutions.ru>
2020-07-12 11:36:19 +0300

    Split test collection assert into two class

---
+++ tests/WS/Utils/Collections/UnitConstraints/CollectionComparingConstraint.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @author Maxim Sokolovsky
+ */
+
+namespace WS\Utils\Collections\UnitConstraints;
+
+use PHPUnit\Framework\Constraint\Constraint;
+use RuntimeException;
+use WS\Utils\Collections\Collection;
+use WS\Utils\Collections\CollectionFactory;
+
+abstract class CollectionComparingConstraint extends Constraint implements StaticCompareCreation
+{
+    private $expectedCollection;
+
+    public function __construct($expectedCollection)
+    {
+        $this->expectedCollection = $this->normalize($expectedCollection);
+    }
+
+    private function normalize($collection)
+    {
+        if (is_array($collection)) {
+            return CollectionFactory::from($collection);
+        }
+
+        return $collection;
+    }
+
+    protected function matches($other): bool
+    {
+        $this->normalize($other);
+        if (!$other instanceof Collection) {
+            throw new RuntimeException('Value of comparision need to be a Collection');
+        }
+
+        return $this->comparingResult($this->expectedCollection, $other);
+    }
+
+    public function toString(): string
+    {
+        return sprintf(
+            'is accepted by %s',
+            $this->exporter()->export($this->expectedCollection)
+        );
+    }
+
+    public static function to($expectedValue): CollectionComparingConstraint
+    {
+        return new static($expectedValue);
+    }
+
+    abstract public function comparingResult(Collection $expectedCollection, Collection $other): bool;
+}
--- tests/WS/Utils/Collections/UnitConstraints/CollectionIsNotEqual.php
+++ tests/WS/Utils/Collections/UnitConstraints/CollectionIsNotEqual.php
@@ -7,11 +7,11 @@ namespace WS\Utils\Collections\UnitConstraints;
 
 use WS\Utils\Collections\Collection;
 
-class CollectionIsEqual extends CollectionComparingConstraint
+class CollectionIsNotEqual extends CollectionComparingConstraint
 {
 
     public function comparingResult(Collection $expectedCollection, Collection $other): bool
     {
-        return $expectedCollection->equals($other);
+        return !$expectedCollection->equals($other);
     }
 }
