5c464535ed439a52b4446717ae3d0b984e6e0c8f
Anton Lytkin <a.lytkin@worksolutions.ru>
2020-10-31 14:27:11 +0300

    Added aggregator tests.

--- tests/WS/Utils/Collections/Functions/Group/GroupTest.php
+++ tests/WS/Utils/Collections/Functions/Group/GroupTest.php
@@ -4,11 +4,12 @@ namespace WS\Utils\Collections\Functions\Group;
 
 use PHPUnit\Framework\TestCase;
 use WS\Utils\Collections\CollectionFactory;
+use WS\Utils\Collections\Functions\Group\Aggregator\Count;
 
 class GroupTest extends TestCase
 {
 
-    public function arrayCases()
+    public function arrayCases(): array
     {
         return [
             [
@@ -161,7 +162,7 @@ class GroupTest extends TestCase
         ];
     }
 
-    public function objectCases() {
+    public function objectCases(): array {
         return [
             [
                 'groupProperty',
@@ -227,6 +228,88 @@ class GroupTest extends TestCase
         ];
     }
 
+    public function aggregateCases(): array
+    {
+        return [
+            [
+                'groupKey',
+                [
+                    [
+                        'firstKey' => 11,
+                        'groupKey' => 'test',
+                        'secondKey' => 78,
+                        'thirdKey' => '14',
+                        'other' => 11,
+                    ],
+                ],
+                [
+                    [
+                        'test' => [
+                            'thirdKey' => 14,
+                        ]
+                    ],
+                    [
+                        'test' => [
+                            'firstKey' => 11,
+                            'secondKey' => 78,
+                        ]
+                    ],
+                    [
+                        'test' => [
+                            'replacedFirstKey' => 78,
+                            'thirdKey' => 14,
+                            'count' => 1,
+                            'replacedLastKey' => 11,
+                        ]
+                    ],
+                ]
+            ],
+            [
+                'groupKey',
+                [
+                    [
+                        'firstKey' => 11,
+                        'groupKey' => 'test',
+                        'secondKey' => 78,
+                        'thirdKey' => '14',
+                        'other' => 11,
+                    ],
+                    new class () {
+                        public $firstKey = 10;
+                        public $secondKey = null;
+                        public $groupKey = 'test';
+
+                        public function getThirdKey()
+                        {
+                            return 190;
+                        }
+                    },
+                ],
+                [
+                    [
+                        'test' => [
+                            'thirdKey' => 190,
+                        ]
+                    ],
+                    [
+                        'test' => [
+                            'firstKey' => 10,
+                            'secondKey' => 78,
+                        ]
+                    ],
+                    [
+                        'test' => [
+                            'replacedFirstKey' => 78,
+                            'thirdKey' => 102,
+                            'count' => 2,
+                            'replacedLastKey' => 10,
+                        ]
+                    ],
+                ]
+            ],
+        ];
+    }
+
     /**
      * @test
      * @dataProvider arrayCases
@@ -244,5 +327,42 @@ class GroupTest extends TestCase
         $this->assertEquals($expected, $result);
     }
 
+    /**
+     * @test
+     * @dataProvider aggregateCases
+     * @param mixed $groupKey
+     * @param array $values
+     * @param array $expected
+     */
+    public function aggregate($groupKey, array $values, array $expected)
+    {
+        $result = CollectionFactory::from($values)
+            ->stream()
+            ->collect(Group::by($groupKey)
+                ->max('thirdKey')
+            );
+
+        $this->assertEquals($result, $expected[0]);
+
+        $result = CollectionFactory::from($values)
+            ->stream()
+            ->collect(Group::by($groupKey)
+                ->min('firstKey')
+                ->sum('secondKey')
+            );
+
+        $this->assertEquals($result, $expected[1]);
+
+        $result = CollectionFactory::from($values)
+            ->stream()
+            ->collect(Group::by($groupKey)
+                ->first('secondKey', 'replacedFirstKey')
+                ->avg('thirdKey')
+                ->addAggregator('count', new Count())
+                ->last('firstKey', 'replacedLastKey')
+            );
+
+        $this->assertEquals($result, $expected[2]);
+    }
 
 }
